a163901b501062ddd39121560277fdd5b4f6d834
Soham-1111 <73647733+Soham-1111@users.noreply.github.com>
2021-04-29 18:17:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,3 +6,33 @@ Webcam.set({
 
 });
 Webcam.attach(" #camera");
+
+function capture(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML= "<img id='captured_image' src=" + data_uri + ">";
+    });
+}
+
+console.log("ml5 version=", ml5.version);
+
+classifier= ml5.imageClassifier("",modelLoaded);
+
+function modelLoaded(){
+    console.log("model successfully loaded");
+}
+
+function identify(){
+    img= document.getElementById('captured_img');
+    Classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if (error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("object").innerHTML= results[0].label;
+        document.getElementById("accuracy").innerHTML= results[0].confidence.toFixed(3);
+    }
+}
